50f7e5977f4f4f178e569ec04d901a4978e93411
khajanawaznakeeb <khajanawaz99@gmail.com>
2021-05-12 16:49:28 +0530

    day6

--- day6/src/components/alubm.js
+++ day6/src/components/alubm.js
@@ -14,6 +14,7 @@ export class Album extends Component {
                 hoverable
                 style={{ width: 240 }}
                 cover={<img alt="example" src="https://th.bing.com/th/id/OIP.O5tUNJsGg-30Evxw7tIzDQHaKl?w=136&h=194&c=7&o=5&dpr=1.5&pid=1.7" width="300" height="380" />}
+                
                 >
                     <Meta title= {this.props.album_title}/>
                     <h5>{this.props.artist}</h5>
